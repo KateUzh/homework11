8e584c0d0502cc3b6ddd00736f18f0ccb901c74d
HomeWorld <KateLusis@gmail.com>
2025-04-11 15:03:55 +0300

    Add homework

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
-import java.time.LocalDate;
-
 public class Main {
     public static boolean isYearLeap(int year) {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year >= 1584;
-
     }
 
     public static void printInfoAboutVersion(int osType, int clientDeviceYear) {
-        int currentYear = LocalDate.now().getYear();
-        if (osType == 0 && clientDeviceYear < currentYear) {
+        int relativeYear = 2015;
+        if (osType == 0 && clientDeviceYear < relativeYear) {
             System.out.println("Установите облегчённую версию для iOS по ссылке");
-        } else if (osType == 0 && clientDeviceYear == currentYear) {
+        } else if (osType == 0 && clientDeviceYear >= relativeYear) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
-        if (osType == 1 && clientDeviceYear < currentYear) {
+        if (osType == 1 && clientDeviceYear < relativeYear) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке");
-        } else if (osType == 1 && clientDeviceYear == currentYear) {
+        } else if (osType == 1 && clientDeviceYear >= relativeYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
@@ -39,14 +36,15 @@ public class Main {
         System.out.println("Задача 1");
         int year = 2021;
         boolean leapYear = isYearLeap(year);
-        if (leapYear)
+        if (leapYear) {
             System.out.println(year + " год високосный");
-        else
+        } else {
             System.out.println(year + " год не високосный");
+        }
 
         System.out.println("\nЗадача 2");
         int osType = 0;
-        int clientDeviceYear = 2020;
+        int clientDeviceYear = 2010;
         printInfoAboutVersion(osType, clientDeviceYear);
 
         System.out.println("\nЗадача 3");
@@ -59,8 +57,10 @@ public class Main {
             System.out.println("Потребуется дней: " + deliveryDays);
         }
         if (deliveryDays == 3) {
-            System.out.println("Потребуется дней: " + deliveryDays);}
+            System.out.println("Потребуется дней: " + deliveryDays);
+        }
         if (deliveryDays == 0) {
-            System.out.println("Доставки нет");}
+            System.out.println("Доставки нет");
+        }
     }
 }
